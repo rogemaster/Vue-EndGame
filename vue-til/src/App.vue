f94fafdf33368410d79c4382644ff422840d0eac
rogemaster <rlaaudwn000@naver.com>
2022-03-28 22:44:11 +0900

    2022-03-28 vue-til project 재 업로드

--- vue-til/src/App.vue
+++ vue-til/src/App.vue
@@ -7,18 +7,25 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
+import AppHeader from './components/common/AppHeader.vue';
+
+var a = 'hi';
 
 export default {
-	name: 'App',
+	name: 'app',
 	components: {
 		HelloWorld,
+		AppHeader,
+	},
+	created() {
+		console.log('hi');
 	},
 };
 </script>
 
 <style>
 #app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
 	text-align: center;
--- vue-til/vue.config.js
+++ vue-til/vue.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-    // 에러발생시 화면에 나오는 오류 메세지 끄는 옵션
-    devServer: {
-        overlay: false
-    }
+	devServer: {
+		overlay: false,
+	},
 };
