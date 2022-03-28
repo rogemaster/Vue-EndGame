ba38a3ed9eb607771ccbc35f13f36fdfb40c7293
rogemaster <rlaaudwn000@naver.com>
2022-03-28 23:22:50 +0900

    2022-03-28 route 설정 및 코드스플린더 처리

---
+++ vue-til/src/routes/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+// component 웹팩 다이나믹 임포트 기능 코드스플리팅 처리
+export default new VueRouter({
+  // mode : history mode, hash mode(default)
+  mode: 'history',
+  routes: [
+    // redirect router 처리
+    {
+      path: '/',
+      redirect: '/login',
+    },
+    {
+      path: '/login',
+      component: () => import('@/views/LoginPage.vue'),
+    },
+    {
+      path: '/signup',
+      component: () => import('@/views/SignupPage.vue'),
+    },
+
+    // Call Back Router 처리
+    {
+      path: '*',
+      component: () => import('@/views/NotFoundPage.vue'),
+    },
+  ],
+});
